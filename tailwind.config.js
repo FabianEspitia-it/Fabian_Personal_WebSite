11a263ea9ae457ed7238b47e3b4a18d05d3b968f
Fabián Espitia <fabianespitia370@gmail.com>
2024-10-11 00:10:06 -0500

    added new projects and added about information

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage : {
-        'extension' : 'url("../img/Extension.png")',
+        'entrepreneurial' : 'url("../img/entrepreneurial.png")',
         'htm': 'url("../img/hack_the_market.png")',
         'fabian': 'url("../img/fabian.png")',
         'devsposts': 'url("../img/devsposts.png")',
         'sunny': 'url("../img/sunny.png")',
         'spotinet': 'url("../img/spotinet.png")',
+        'coding': 'url("../img/coding.png")'
 
       },
       fontFamily: {
