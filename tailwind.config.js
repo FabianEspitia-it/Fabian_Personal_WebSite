ec5847cd1b7e94f56688d97de5880d703d324ab5
FabianEspitia-it <fabianespitia370@gmail.com>
2025-05-21 00:31:55 -0500

    add naurat project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
-      backgroundImage : {
-        'entrepreneurial' : 'url("../img/entrepreneurial.png")',
-        'htm': 'url("../img/hack_the_market.png")',
-        'fabian': 'url("../img/fabian.png")',
-        'devsposts': 'url("../img/devsposts.png")',
-        'sunny': 'url("../img/sunny.png")',
-        'spotinet': 'url("../img/spotinet.png")',
-        'coding': 'url("../img/coding.png")'
-
+      backgroundImage: {
+        entrepreneurial: 'url("../img/entrepreneurial.png")',
+        htm: 'url("../img/hack_the_market.png")',
+        fabian: 'url("../img/fabian.png")',
+        devsposts: 'url("../img/devsposts.png")',
+        sunny: 'url("../img/sunny.png")',
+        spotinet: 'url("../img/spotinet.png")',
+        coding: 'url("../img/coding.png")',
+        naurat: 'url("../img/naurat.png")',
       },
       fontFamily: {
-        'source-code-pro': ['Source Code Pro', 'monospace'],
-      }
+        "source-code-pro": ["Source Code Pro", "monospace"],
+      },
     },
   },
   plugins: [],
-}
+};
